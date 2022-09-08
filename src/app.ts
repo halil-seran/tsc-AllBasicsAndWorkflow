1ded6d5a993649550ec3b385a3a375c73b8ebe57
Halil <halil.seran.1135@gmail.com>
2022-09-08 22:38:27 +0300

    letsgo

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
-class Department {
+abstract class Department {
+  static fiscalYear = 2020;
   //   private readonly id: string;
   //   public name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+    console.log(Department.fiscalYear);
   }
 
   static createEmployee(name: string) {
     return { name: name };
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
+  abstract describe(this: Department): void;
+  //   {
+  //     // console.log(`Department (${this.id}): ${this.name}`);
+  //   }
 
   addEmployee(employee: string) {
     // this.id = "d1";
@@ -33,10 +36,15 @@ class ITDepartment extends Department {
     super(id, "IT");
     this.admins = admins;
   }
+
+  describe() {
+    console.log("IT Department - ID: " + this.id);
+  }
 }
 
 class AccountingDepartment extends Department {
   private lastReport: string;
+  private static instance: AccountingDepartment;
 
   get mostRecentReport() {
     if (this.lastReport) {
@@ -52,9 +60,21 @@ class AccountingDepartment extends Department {
     this.addReport(value);
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
     this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment("d2", []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log("Accounting Department - ID:" + this.id);
   }
 
   addEmployee(name: string) {
@@ -85,7 +105,7 @@ class AccountingDepartment extends Department {
 // accounting.printEmployeeInformation();
 
 const employee1 = Department.createEmployee("Ummu");
-console.log(employee1);
+console.log(employee1, Department.fiscalYear);
 
 const it = new ITDepartment("b1", ["Halil"]);
 
@@ -101,7 +121,12 @@ console.log(it);
 
 // accountingCopy.describe();
 
-const accounting = new AccountingDepartment("d2", []);
+// const accounting = new AccountingDepartment("d2", []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+//these are the same objects because of singleton
+
+console.log(accounting, accounting2);
 
 accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong!");
@@ -110,5 +135,7 @@ console.log(accounting.mostRecentReport);
 accounting.addEmployee("Halil");
 accounting.addEmployee("Halill");
 
-accounting.printReports();
-accounting.printEmployeeInformation();
+// accounting.printReports();
+// accounting.printEmployeeInformation();
+
+accounting.describe();
