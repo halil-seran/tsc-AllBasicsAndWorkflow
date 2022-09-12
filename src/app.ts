6c538ea9b52e660fd8deb79950cc13bfcfe601e0
Halil <halil.seran.1135@gmail.com>
2022-09-12 22:10:58 +0300

    monday

--- src/app.ts
+++ src/app.ts
@@ -1,150 +1,23 @@
-type Admin = {
-  name: string;
-  privileges: string[];
-};
+/*const names: Array<string> = ["Halil", "Ummu"]; // string[] also we can use string | number
+names[0].split(" "); //we can do all strings stuff because we know array is string
 
-interface Employee {
-  name: string;
-  startDate: Date;
-}
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
 
-// interface ElevatedEmployee extends Employee,Admin{
-//interface ile cok benzer
-// }
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-  name: "halil",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
-
-type Combinable = string | number;
-type Numberic = number | boolean;
-type Universal = Combinable & Numberic;
-
-function adding(a: number, b: number): number;
-function adding(a: string, b: string): string;
-function adding(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
-
-const result = adding("Halil", "Max") as string;
-result.split(" ");
-
-const fetchedUserData = {
-  id: "u1",
-  name: "Halil",
-  job: { title: "CEO", description: "My own company" },
-};
-
-console.log(fetchedUserData?.job?.title);
-//bu bir if check eger fetchData varsa job, job varsa titlei yaz anlaminda
-// console.log(fetchedUserData.job && fetchedUserData.job.title) js alternatifi fakat ts de ? koyarak if checke alabiliyoruz
-
-const userInput = "";
-
-//const storedData = userInput || "DEFAULT"; //if emptysting or null or undefined use DEFAULT
-const storedData = userInput ?? "DEFAULT"; //if null or undefined use DEFAULT
-
-console.log(storedData);
-
-/*
-type UnknownEmployee = Employee | Admin;
-
-function printEmployeeInfo(emp: UnknownEmployee) {
-  console.log("Name: " + emp.name);
-  if ("privileges" in emp) {
-    //this is check privileges emp in icinde var mi
-    console.log("Privileges: " + emp.privileges);
-  }
-  if ("startDate" in emp) {
-    console.log("StartDate: " + emp.startDate);
-  }
-}
-
-printEmployeeInfo(e1);
-
-class Car {
-  drive() {
-    console.log("Driving...");
-  }
-}
-
-class Truck {
-  drive() {
-    console.log("Driving a truck...");
-  }
-
-  loadCargo(amount: number) {
-    console.log("Loading Cargo..." + amount);
-  }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  //   if ("loadCargo" in vehicle) { this is also work
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-}
-
-useVehicle(v1);
-useVehicle(v2);
-
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-  }
-  console.log("Moving with speed: " + speed);
-}
-
-moveAnimal({ type: "bird", flyingSpeed: 70 });
-
-// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
-// const userInputElement = document.getElementById("user-input")! as HTMLInputElement;
-// userInputElement.value = "Hi There!";
-
-const userInputElement = document.getElementById("user-input")!;
-
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = "Hi There!";
-}
-
-interface ErrorContainer {
-  id: string;
-  [prop: string]: string;
-}
-
-const errorBag: ErrorContainer = {
-    email:'Not a valid email!',
-    username:"Must start with a character!"
-};
+promise.then(data =>{
+    data.split(" ")
+})
 */
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+//extends is forcing to be objects // they cant be 30
+console.log(merge({ name: "Halil" }, { age: 24 }));
+
+const mergedObj = merge({ name: "Halil" }, { age: 24 });
+
+console.log(mergedObj.name);
