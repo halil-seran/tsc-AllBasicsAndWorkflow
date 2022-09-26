038012309ed97240db468fc9fb8643fa23df5b4b
Halil <halil.seran.1135@gmail.com>
2022-09-26 22:24:28 +0300

    summary

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,32 @@ bool = true;
 
 //more Complex types
 
+//default type = any
+
 let hobbies: string[]; // array of strings
+hobbies = ["armwrestling", "sport"];
+
+let person: { name: string; age: number };
+
+person = {
+  name: "halil",
+  age: 24,
+};
+
+let people: { name: string; age: number }[]; //persons array
+
+//type inference
+
+let course = "React Native"; //string yazmamiza gerek yok kendi cikarim yapip string dedi
+
+// course = 12; ERROR!
+
+let course2: string | number = "React";
+
+course2 = 24;
+
+type PersonType = {
+  name: string;
+  age: number;
+};
+// now we created our type we can use it anywhere like other types
