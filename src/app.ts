01290806bfecddaa56b5eba98594655aae6e023c
Halil <halil.seran.1135@gmail.com>
2022-09-26 22:03:23 +0300

    summary

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("chance the name of file to app.ts that you wanna run and delete me");
+console.log(
+  "chance the name of file to app.ts that you wanna run and delete me"
+);
+
+//primitives
+
+let userId: number;
+userId = 10;
+
+let userName: string;
+userName = "Halil";
+
+let bool: boolean;
+bool = true;
+
+//more Complex types
+
+let hobbies: string[]; // array of strings
